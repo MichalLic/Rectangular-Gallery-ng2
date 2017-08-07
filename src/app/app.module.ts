cb41bbae7e9ff765faa2bd30e00d3e9f05692311
Michal <m14lickiewicz@gmail.com>
2017-08-07 11:56:02 +0200

    Prepate project environment.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { OutputComponent } from './output/output.component';
+import { EditorComponent } from './editor/editor.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutputComponent,
+    EditorComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
