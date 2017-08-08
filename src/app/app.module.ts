f5fe7c1495623a58c42c7a3bf6dcb19df4e7696f
Michal <m14lickiewicz@gmail.com>
2017-08-08 11:50:27 +0200

    Add routing and upload files method.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
+import {ImageUploadModule} from 'angular2-image-upload';
 
-import { AppComponent } from './app.component';
-import { OutputComponent } from './output/output.component';
-import { EditorComponent } from './editor/editor.component';
-import { GalleryComponent } from './gallery/gallery.component';
+import {AppComponent} from './app.component';
+import {OutputComponent} from './output/output.component';
+import {EditorComponent} from './editor/editor.component';
+import {GalleryComponent} from './gallery/gallery.component';
+import {HeaderComponent} from './header/header.component';
+import {AngularFireModule} from 'angularfire2';
+
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'output', pathMatch: 'full'},
+  {path: 'output', component: OutputComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     OutputComponent,
     EditorComponent,
-    GalleryComponent
+    GalleryComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    ImageUploadModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-output',
   templateUrl: './output.component.html',
-  styleUrls: ['./output.component.css']
+  styleUrls: ['./output.component.css'],
 })
-export class OutputComponent implements OnInit {
 
-  constructor() { }
+
+export class OutputComponent implements OnInit {
+  fileName: string;
+  showImage = false;
+  url: string;
+  firebaseConfig = {
+    apiKey: 'AIzaSyCQBlpqtOGwn4ujwTTj90NYZ4e-wNnz5w4',
+    authDomain: 'rectangulargallery.firebaseapp.com',
+    databaseURL: 'https://rectangulargallery.firebaseio.com',
+    projectId: 'rectangulargallery',
+    storageBucket: 'rectangulargallery.appspot.com',
+    messagingSenderId: '570084892486'
+  };
+
+
+  constructor() {
+  }
+
 
   ngOnInit() {
+    firebase.initializeApp(this.firebaseConfig);
+  }
+
+  changeListner(event) {
+    console.log(event.target.files[0]);
+    const file = event.target.files[0];
+    const metadata = {
+      contentType: 'image/jpeg'
+    };
+
+    firebase.storage().ref().child('images/' + file.name).put(file, metadata);
+
+    firebase.storage().ref()
+      .child('images/' + file.name)
+      .getDownloadURL().then(url => {
+      console.log(url);
+      this.url = url;
+    });
+    this.showImage = true;
   }
 
 }
